ce772215a375c57cb2f3d133e4a23800d3d0fda1
OkonFavour <nsikanokon13@gmail.com>
2023-08-16 02:36:02 +0100

    Push na

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ReactProjectTask1/",
+  base: "/React-Project-Task1/",
   plugins: [react()],
 })
